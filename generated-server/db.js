417cd37f61ae4e0eaf53de4ed58f4566ae1ab2d7
adrrf <adrf.dev@gmail.com>
2025-01-17 17:32:05 +0100

    feat: added nedb

---
+++ generated-server/db.js
@@ -0,0 +1,155 @@
+"use strict";
+
+const Datastore = require("nedb");
+const assert = require("assert");
+const path = require("path");
+
+const dbPath = "data/";
+const dbFileName = path.join(__dirname, dbPath, "contacts.db");
+
+let dbInstance;
+
+/**
+ * Connects to the database and initializes the connection if not already established.
+ * @param {Function} callback - Callback function to handle the connection.
+ */
+function connect(callback) {
+  if (dbInstance) {
+    console.warn("Database connection already exists!");
+    return callback(null, dbInstance);
+  }
+
+  dbInstance = new Datastore({
+    filename: dbFileName,
+    autoload: true,
+  });
+  return callback(null, dbInstance);
+}
+
+/**
+ * Retrieves the current database connection.
+ * @returns {Datastore} The current database instance.
+ * @throws Will throw an error if the connection has not been established.
+ */
+function getConnection() {
+  assert.ok(
+    dbInstance,
+    "Database connection not established. Please call connect() first.",
+  );
+  return dbInstance;
+}
+
+/**
+ * Initializes the database with sample data.
+ * @returns {Promise} Resolves when the sample data is inserted.
+ */
+async function init() {
+  const sampleFragrances = [
+    {
+      name: "Creeed Aventus",
+      brand: "Creeed",
+      type: "Eau de Parfum",
+      notes: {
+        top: ["Pineapple", "Bergamot", "Blackcurrant"],
+        middle: ["Birch", "Patchouli", "Rose"],
+        base: ["Musk", "Oakmoss", "Vanilla"],
+      },
+      gender: "Masculine",
+      release_year: 2010,
+      owned: true,
+    },
+    {
+      name: "Tom Ford Oud Wood",
+      brand: "Tom Ford",
+      type: "Eau de Parfum",
+      notes: {
+        top: ["Rosewood", "Cardamom", "Chinese pepper"],
+        middle: ["Oud", "Sandalwood", "Vetiver"],
+        base: ["Tonka bean", "Vanilla", "Amber"],
+      },
+      gender: "Masculine",
+      release_year: 2007,
+      owned: true,
+    },
+    {
+      name: "Layton",
+      brand: "Parfums de Marly",
+      type: "Eau de Parfum",
+      notes: {
+        top: ["Apple", "Bergamot", "Lavender"],
+        middle: ["Jasmine", "Geranium", "Violet"],
+        base: ["Vanilla", "Sandalwood", "Cardamom"],
+      },
+      gender: "Masculine",
+      release_year: 2016,
+      owned: false,
+    },
+    {
+      name: "Baccarat Rouge 540",
+      brand: "Maison Francis Kurkdjian",
+      type: "Eau de Parfum",
+      notes: {
+        top: ["Saffron", "Jasmine", "Amberwood"],
+        middle: ["Ambergris", "Fir resin", "Cedar"],
+        base: ["Musk", "Oakmoss", "Vanilla"],
+      },
+      gender: "Unisex",
+      release_year: 2015,
+      owned: false,
+    },
+  ];
+
+  return new Promise((resolve, reject) => {
+    getConnection().insert(sampleFragrances, (err, docs) => {
+      if (err) return reject(err);
+      resolve(docs);
+    });
+  });
+}
+
+/**
+ * Executes a query on the database.
+ * @param {Object} query - The query object.
+ * @param {Function} callback - Callback function to handle the results.
+ */
+function find(query, callback) {
+  getConnection().find(query, callback);
+}
+
+/**
+ * Inserts a new document into the database.
+ * @param {Object} doc - The document to insert.
+ * @param {Function} callback - Callback function to handle the result.
+ */
+function insert(doc, callback) {
+  getConnection().insert(doc, callback);
+}
+
+/**
+ * Updates a document in the database that matches the query.
+ * @param {Object} query - The query object.
+ * @param {Object} newDoc - The new document values.
+ * @param {Function} callback - Callback function to handle the result.
+ */
+function update(query, newDoc, callback) {
+  getConnection().update(query, newDoc, callback);
+}
+
+/**
+ * Removes a document from the database.
+ * @param {Object} query - The query object.
+ * @param {Function} callback - Callback function to handle the result.
+ */
+function remove(query, callback) {
+  getConnection().remove(query, callback);
+}
+
+module.exports = {
+  connect,
+  getConnection,
+  init,
+  find,
+  insert,
+  update,
+  remove,
+};
--- generated-server/index.js
+++ generated-server/index.js
@@ -1,22 +1,69 @@
-const http = require('http');
+const http = require("http");
 const express = require("express");
-const { initialize } = require('@oas-tools/core');
-
+const { initialize } = require("@oas-tools/core");
 
 const serverPort = 8080;
 const app = express();
-app.use(express.json({limit: '50mb'}));
 
-const config = {}
+app.use(express.json({ limit: "50mb" }));
 
+const config = {
+  oasFile: "./api/oas-doc.yaml",
+  middleware: {
+    security: {
+      auth: {},
+    },
+  },
+};
+
+const db = require("./db");
+
+// Initialize database connection
+(async () => {
+  try {
+    console.info("Initializing DB...");
+
+    const _db = await new Promise((resolve, reject) => {
+      db.connect((err, dbInstance) => {
+        if (err) return reject(err);
+        resolve(dbInstance);
+      });
+    });
+
+    const contacts = await new Promise((resolve, reject) => {
+      db.find({}, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+
+    if (contacts.length === 0) {
+      console.info("Empty DB, loading initial data...");
+      db.init();
+    } else {
+      console.info(`DB already has ${contacts.length} contacts.`);
+    }
+  } catch (error) {
+    console.error("Error during DB initialization!", error);
+  }
+})();
 
 initialize(app, config).then(() => {
-    http.createServer(app).listen(serverPort, () => {
-    console.log("\nApp running at http://localhost:" + serverPort);
-    console.log("________________________________________________________________");
+  const server = http.createServer(app);
+
+  server.listen(serverPort, () => {
+    console.log(`\nApp running at http://localhost:${serverPort}`);
+    console.log(
+      "________________________________________________________________",
+    );
+
     if (!config?.middleware?.swagger?.disable) {
-        console.log('API docs (Swagger UI) available on http://localhost:' + serverPort + '/docs');
-        console.log("________________________________________________________________");
+      console.log(
+        `API docs (Swagger UI) available on http://localhost:${serverPort}/docs`,
+      );
+      console.log(
+        "________________________________________________________________",
+      );
     }
-    });
+  });
 });
